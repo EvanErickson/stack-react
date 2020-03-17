855199360a53a206c90504dc3d63926bd98c8ac9
fbuitron <franciscobuitron21@gmail.com>
2020-03-17 13:48:13 -0700

    initial commit and card on webpage

---
+++ src/components/Rightside/Rightside.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Rightside.css'
+
+const Rightside = () => {
+  return (
+    <div>
+      <div className="card" style-prop-object="width: 18rem;">
+        <div className="card-header">
+          Featured
+        </div>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">Cras justo odio</li>
+          <li className="list-group-item">Dapibus ac facilisis in</li>
+          <li className="list-group-item">Vestibulum at eros</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Rightside
