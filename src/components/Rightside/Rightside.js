280c9691c3ae1af313b523e2f382fd5a0f1477c5
fbuitron <franciscobuitron21@gmail.com>
2020-03-17 14:13:13 -0700

    moved entire column to the right

--- src/components/Rightside/Rightside.js
+++ src/components/Rightside/Rightside.js
@@ -4,31 +4,33 @@ import './Rightside.css'
 const Rightside = () => {
   return (
     <div>
-      <div className="card col-md-10">
-        <div className="card-header">
-          Overflow Blog
+      <div className="col-md-10 right">
+        <div className="card">
+          <div className="card-header">
+            Overflow Blog
+          </div>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">Cras justo odio</li>
+            <li className="list-group-item">Dapibus ac facilisis in</li>
+            <li className="list-group-item">Vestibulum at eros</li>
+          </ul>
+          </div>
+        <div className="card">
+          <div className="card-header">
+            Featured
+          </div>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">Cras justo odio</li>
+            <li className="list-group-item">Dapibus ac facilisis in</li>
+            <li className="list-group-item">Vestibulum at eros</li>
+          </ul>
         </div>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">Cras justo odio</li>
-          <li className="list-group-item">Dapibus ac facilisis in</li>
-          <li className="list-group-item">Vestibulum at eros</li>
-        </ul>
-      </div>
-      <div className="card col-md-10">
-        <div className="card-header">
-          Featured
-        </div>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">Cras justo odio</li>
-          <li className="list-group-item">Dapibus ac facilisis in</li>
-          <li className="list-group-item">Vestibulum at eros</li>
-        </ul>
-      </div>
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">Special title treatment</h5>
-          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Watched Tags</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="btn btn-primary">Watch a tag</a>
+          </div>
         </div>
       </div>
     </div>
