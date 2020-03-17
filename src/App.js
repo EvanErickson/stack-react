c1d2050ec37cf487415311feab1ad43e92ab1fa1
Aria Sadeghi <ariasadeghi@gmail.com>
2020-03-17 13:51:13 -0700

    put it in the folder

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import Question from './components/Question';
+import Question from './components/questions/Question';
 import Navbar from './components/Navbar';
 
 function App() {
+let arr = new Array();
+arr.length = 10;
   return (
     <>
     <Navbar />
     <div className="container">
       <div className="row">
-    <Question />
+          {arr.map(elem => { <Question elm={elem}/>})}
     </div>
     </div>
     </>
